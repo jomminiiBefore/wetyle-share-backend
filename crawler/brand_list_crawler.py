898247ad38a4b71d6133bf61e8cfa82124df1f45
이종민 <>
2020-02-25 21:00:13 +0900

    made brand name, img, description crawler

--- crawler/brand_list_crawler.py
+++ crawler/brand_list_crawler.py
@@ -6,20 +6,17 @@ from selenium.webdriver.common.keys import Keys
 
 url = 'https://www.styleshare.kr/brands'
 
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-options.add_argument('disable-gpu')
-
-driver = webdriver.Chrome('/Applications/chromedriver'), options=options)
+driver = webdriver.Chrome(('/Applications/chromedriver'))
 driver.get(url)
 
 brand_list = []
 
-for page in range(1,17):
+for page in range(1,4):
     driver.implicitly_wait(5)
+
     if page == 1:
         pass
-
+    
     else :
         elements = driver.find_element_by_xpath(f'//*[@id="app"]/div/div[2]/div[1]/div/button[{page}]')
         driver.execute_script("arguments[0].click();", elements)
@@ -40,19 +37,22 @@ for page in range(1,17):
 
         # Calculate new scroll height and compare with last scroll height
         new_height = driver.execute_script("return document.body.scrollHeight")
+
         if new_height == last_height:
             break
 
         last_height = new_height
 
-    brandImg = driver.find_elements_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div[1]/div/a/img')
+    brandImg  = driver.find_elements_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div[1]/div/a/img')
     brandName = driver.find_elements_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div[1]/div/div/div/a')
+    brandLink = driver.find_elements_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div[1]/div/a')
 
-    for item in zip(brandName,brandImg):
+    for item in zip(brandName,brandImg,brandLink):
         brand_list.append(
             {
                 "name" : item[0].text,
                 "img"  : item[1].get_attribute("src"),
+                "link" : item[2].get_attribute("href"),
             }
         )
 
@@ -60,6 +60,6 @@ with open('./brand_infos.csv', mode='w') as brand_infos:
     brand_writer = csv.writer(brand_infos)
 
     for list in brand_list:
-        brand_writer.writerow([list["name"], list["img"]])
+        brand_writer.writerow([list["name"], list["img"], list["link"]])
 
 driver.quit()
