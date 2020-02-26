56e46ae13846fb4f15047e9a91eb43b8925f23b9
이종민 <>
2020-02-26 13:05:05 +0900

    피드백 반영 수정, 대부분space

--- user/views.py
+++ user/views.py
@@ -16,17 +16,16 @@ class SignUpView(View):
         data = json.loads(request.body)
         try:
             if User.objects.filter(login_id = data['login_id']).exists():
-                return JsonResponse({"message": "existing login_id"}, status = 400)
+                return JsonResponse({"message":"existing login_id"}, status = 400)
 
             if len(data['password']) < 7:
-                return JsonResponse({"message": "too short password"}, status = 400)
+                return JsonResponse({"message":"too short password"}, status = 400)
 
             password_crypt = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
-
             validate_email(data['email'])
 
             if User.objects.filter(email = data['email']).exists():
-                return JsonResponse({"message": "existing email"}, status = 400)
+                return JsonResponse({"message":"existing email"}, status = 400)
 
             User(
                 login_id  = data['login_id'],
@@ -38,15 +37,12 @@ class SignUpView(View):
                 image_url = data.get('image_url',None),
             ).save()
 
-            token = jwt.encode({'login_id' : data['login_id']}, SECRET_KEY, algorithm = 'HS256')
-
-            return JsonResponse({"token" : token.decode('utf-8')}, status = 200)
-
+            token = jwt.encode({'login_id':data['login_id']}, SECRET_KEY, algorithm = 'HS256')
+            return JsonResponse({"token":token.decode('utf-8')}, status = 200)
         except ValidationError:
-            return JsonResponse({"message":"INVALID_EMAIL"}, status=400)
-
+            return JsonResponse({"message":"INVALID_EMAIL"}, status = 400)
         except KeyError:
-            return JsonResponse({"message": "INVALID_KEYS"}, status = 400)
+            return JsonResponse({"message":"INVALID_KEYS"}, status = 400)
 
 class SignInView(View):
     def post(self, request):
@@ -57,15 +53,9 @@ class SignInView(View):
                user = User.objects.get(login_id = data['login_id'])
 
                if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
-
-                   token = jwt.encode({'login_id' : user.login_id}, SECRET_KEY, algorithm = 'HS256')
-                   token = token.decode('utf-8')
-
-                   return JsonResponse({"token" : token}, status = 200)
-
-               else :
-                   return JsonResponse({"message": "INVALID_PASSWORD"}, status = 401)
-
-        except KeyError :
-             return JsonResponse({"message": "INVALID_KEYS"}, status = 400)
+                   token = jwt.encode({'login_id':user.login_id}, SECRET_KEY, algorithm = 'HS256')
+                   return JsonResponse({"token":token.decode('utf-8')}, status = 200)
+               return HttpResponse(status = 401)
+        except KeyError:
+             return JsonResponse({"message":"INVALID_KEYS"}, status = 400)
 
