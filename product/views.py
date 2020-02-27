f29299a71694876248bb2a4d4acdb14d9265094a
이종민 <>
2020-02-27 10:55:48 +0900

    space modified

--- product/views.py
+++ product/views.py
@@ -9,7 +9,7 @@ from django.http import JsonResponse, HttpResponse
 class BrandListView(View):
     def get(self, request):
         result = []
-        
+
         for i in Brand.objects.values():
             result.append(i)
 
