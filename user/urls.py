27887fd9930c9f28f25e646228fd8317a9bf7d31
yongjunlee <chesidyong@gmail.com>
2020-02-27 10:38:26 +0900

    feat: Add url of CheckIdView, CheckEmailView
    회원가입 시 유저가 아이디와 이메일을 한 글자씩 입력할 때마다 기존 저장 데이터와 중복여부를 확인하기 위한 뷰의 url 주소를 추가했습니다.

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
-from user.views import SignUpView
+from user.views import SignUpView, CheckIdView, CheckEmailView
 from django.urls import path
 
 urlpatterns = [
     path('sign-up/', SignUpView.as_view()),
+    path('sign-up/checkid', CheckIdView.as_view()),
+    path('sign-up/checkemail', CheckEmailView.as_view()),
 ]
 
