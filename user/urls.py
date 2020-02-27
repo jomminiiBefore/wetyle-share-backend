f7bf50e849af8ff0f97ceea2170bbe458880fe9f
yongjunlee <chesidyong@gmail.com>
2020-02-27 19:43:37 +0900

    Fix url path

--- user/urls.py
+++ user/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('sign-up/', SignUpView.as_view()),
     path('sign-up/checkid', CheckIdView.as_view()),
     path('sign-up/checkemail', CheckEmailView.as_view()),
-    path('/sign-in', SignInView.as_view()),
+    path('sign-in/', SignInView.as_view()),
 ]
 
