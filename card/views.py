e6d90ae6c2256a455e564fbcd5f3f5ab886f581b
이종민 <>
2020-02-29 20:12:07 +0900

    related items 추가, comment count 추가, try except 추가

--- card/views.py
+++ card/views.py
@@ -10,27 +10,28 @@ from django.http            import JsonResponse
 
 class StyleView(View):
     def get(self, request, style_id):
-        request_style = Style.objects.get(id = style_id)
-        style_user    = Style.objects.select_related('user').get(id = style_id).user
-        comment       = Style.objects.prefetch_related('comments')\
-                        .select_related('user').get(id = style_id).comments
-        comment_list  = [
-            {
-               'profile_image' : comment.user.image_url,
-               'nickname' : comment.user.nickname,
-               'description' : comment.description,
-               'date' : str(comment.updated_at)[2:11],
-            }
-            for comment in comment.all()]
-        style = [
-            {
-                'style_image_url'  : request_style.image_url,
-                'description': request_style.description,
-                'profile_image_url' : style_user.image_url,
-                'nickname' : style_user.nickname,
-                'profile_description' : style_user.description,
-                'like_count' : StyleLike.objects.filter(style_id = style_id).count(),
-                'comment' : comment_list,
-                },
-        ]
-        return JsonResponse({"result": style}, status = 200)
+        try:
+            request_style = Style.objects.get(id = style_id)
+            style_user    = Style.objects.select_related('user').get(id = style_id).user
+            comments       = Style.objects.prefetch_related('comments')\
+                            .select_related('user').get(id = style_id).comments
+            style = [
+                {
+                    'style_image_url'     : request_style.image_url,
+                    'description'         : request_style.description,
+                    'profile_image_url'   : style_user.image_url,
+                    'nickname'            : style_user.nickname,
+                    'profile_description' : style_user.description,
+                    'like_count'          : StyleLike.objects.filter(style_id = style_id).count(),
+                    'comment_count'       : comments.all().count(),
+                    'comment'             : [
+                        {
+                            'profile_image' : comment.user.image_url,
+                            'nickname'      : comment.user.nickname,
+                            'description'   : comment.description,
+                            'date'          : str(comment.updated_at)[2:11],
+                            } for comment in comments.all()]
+                    }]
+            return JsonResponse({"result": style}, status = 200)
+        except Style.DoesNotExist:
+            return JsonResponse({"message": "INVALID_STYLE_ID"}, status = 400)
