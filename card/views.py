89bf39ff71374c0f5c4f8e89d9081486a4b2e856
이종민 <>
2020-03-01 14:51:06 +0900

    style에서 리스트를 삭제해 프론트에서 접근하기 쉽게 함

--- card/views.py
+++ card/views.py
@@ -13,26 +13,25 @@ class StyleView(View):
         try:
             request_style = Style.objects.get(id = style_id)
             style_user    = Style.objects.select_related('user').get(id = style_id).user
-            comments       = Style.objects.prefetch_related('comments')\
+            comments      = Style.objects.prefetch_related('comments')\
                             .select_related('user').get(id = style_id).comments
-            style = [
-                {
-                    'style_image_url'     : request_style.image_url,
-                    'related_item' : list(request_style.style_related_items.values()),
-                    'description'         : request_style.description,
-                    'profile_image_url'   : style_user.image_url,
-                    'nickname'            : style_user.nickname,
-                    'profile_description' : style_user.description,
-                    'like_count'          : StyleLike.objects.filter(style_id = style_id).count(),
-                    'comment_count'       : comments.all().count(),
-                    'comment'             : [
-                        {
-                            'profile_image' : comment.user.image_url,
-                            'nickname'      : comment.user.nickname,
-                            'description'   : comment.description,
-                            'date'          : str(comment.updated_at)[2:11],
-                            } for comment in comments.all()]
-                    }]
+            style = {
+                'style_image_url'     : request_style.image_url,
+                'related_item'        : list(request_style.style_related_items.values()),
+                'description'         : request_style.description,
+                'profile_image_url'   : style_user.image_url,
+                'nickname'            : style_user.nickname,
+                'profile_description' : style_user.description,
+                'like_count'          : StyleLike.objects.filter(style_id = style_id).count(),
+                'comment_count'       : comments.all().count(),
+                'comment'             : [
+                    {
+                        'profile_image' : comment.user.image_url,
+                        'nickname'      : comment.user.nickname,
+                        'description'   : comment.description,
+                        'date'          : str(comment.updated_at)[2:11],
+                        } for comment in comments.all()]
+                }
             return JsonResponse({"result": style}, status = 200)
         except Style.DoesNotExist:
             return JsonResponse({"message": "INVALID_STYLE_ID"}, status = 400)
