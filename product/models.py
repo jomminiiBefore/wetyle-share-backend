96eedc99d85e48e237a973809f21cd42908fc1cf
yongjunlee <chesidyong@gmail.com>
2020-03-03 09:39:04 +0900

    Feature/insert product detail image data
    제품 모델링이 이미지 테이블을 따로 만들도록 바뀌어서
    데이터, 데이터 삽입코드 작성, 모델링 코드 변경했습니다

--- product/models.py
+++ product/models.py
@@ -2,21 +2,21 @@ from django.db import models
 from user.models import User
 
 class Product(models.Model):
-    name            = models.CharField(max_length = 50)
-    image_url       = models.URLField(max_length = 2000)
+    name            = models.TextField()    
     first_category  = models.ForeignKey('FirstCategory', on_delete = models.SET_NULL, null = True)
     second_category = models.ForeignKey('SecondCategory', on_delete = models.SET_NULL, null = True)
     third_category  = models.ForeignKey('ThirdCategory', on_delete = models.SET_NULL, null = True)
-    brand           = models.ForeignKey('Brand', on_delete = models.SET_NULL, null = True)
+    brand           = models.ForeignKey('Brand', on_delete = models.SET_NULL, related_name = 'brands', null = True)
     price           = models.IntegerField(default = 0)
     discounted_price= models.IntegerField(default = 0)
     point           = models.IntegerField(default = 0)
     detailed_info   = models.TextField()
-    add_info        = models.CharField(max_length = 1000)
+    add_info        = models.TextField()
     created_at      = models.DateTimeField(auto_now_add = True)
     updated_at      = models.DateTimeField(auto_now = True)
     product_color   = models.ManyToManyField('Color', through = 'ProductColor')
-    procuct_size    = models.ManyToManyField('Size', through = 'ProductSize')
+    product_size    = models.ManyToManyField('Size', through = 'ProductSize')
+    product_like    = models.ManyToManyField(User, through = 'ProductLike')
 
     class Meta:
         db_table = 'products'
@@ -36,7 +36,7 @@ class SecondCategory(models.Model):
 
 class ThirdCategory(models.Model):
     name            = models.CharField(max_length = 50)
-    fist_category   = models.ForeignKey('FirstCategory', on_delete = models.SET_NULL, null = True)
+    first_category   = models.ForeignKey('FirstCategory', on_delete = models.SET_NULL, null = True)
     second_category = models.ForeignKey('SecondCategory', on_delete = models.SET_NULL, null = True)
 
     class Meta:
@@ -46,16 +46,23 @@ class Brand(models.Model):
     name            = models.CharField(max_length = 50)
     description     = models.CharField(max_length = 500)
     created_at      = models.DateTimeField(auto_now_add = True)
-    updated_at      = models.DateTimeField(auto_now = True)
+    updated_at      = models.DateTimeField(auto_now = True)    
     small_image_url = models.URLField(max_length = 2000)
     large_image_url = models.URLField(max_length = 2000)
-    
+
     class Meta:
         db_table = 'brands'
 
+class ProductDetailImage(models.Model):
+    image_url       = models.URLField(max_length = 2000)
+    product         = models.ForeignKey('Product', on_delete = models.CASCADE)
+
+    class Meta:
+        db_table = 'product_detail_images'
+
 class ProductLike(models.Model):
     product         = models.ForeignKey('Product', on_delete = models.CASCADE)
-    user            = models.ForeignKey('User', on_delete = models.CASCADE)
+    user            = models.ForeignKey(User, on_delete = models.CASCADE)
     created_at      = models.DateTimeField(auto_now_add = True)
     
     class Meta:
@@ -63,7 +70,7 @@ class ProductLike(models.Model):
 
 class ProductInqury(models.Model):
     decription      = models.CharField(max_length = 500)
-    user            = models.ForeignKey('User', on_delete = models.SET_NULL, null = True)
+    user            = models.ForeignKey(User, on_delete = models.SET_NULL, null = True)
     is_answer       = models.BooleanField(default = False)
     description     = models.CharField(max_length = 500)
     product         = models.ForeignKey('Product', on_delete = models.CASCADE, null = True)
@@ -98,7 +105,7 @@ class Size(models.Model):
         db_table = 'sizes'
 
 class ProductStock(models.Model):
-    product         = models.ForeignKey('Product', on_delete = models.SET_NULL)
+    product         = models.ForeignKey('Product', on_delete = models.SET_NULL, null = True)
     color           = models.ForeignKey('Color', on_delete = models.SET_NULL, null = True)
     size            = models.ForeignKey('Size', on_delete = models.SET_NULL, null = True)
     stock           = models.IntegerField()
@@ -107,7 +114,7 @@ class ProductStock(models.Model):
         db_table = 'product_stocks'
 
 class OrderedProduct(models.Model):
-    order           = models.ForeignKey('Order', on_delete = models.SET_NULL)
+    order           = models.ForeignKey('Order', on_delete = models.SET_NULL, null = True)
     product         = models.ForeignKey('Product', on_delete = models.SET_NULL, null = True)
     size            = models.ForeignKey('Size', on_delete = models.SET_NULL, null = True)
     color           = models.ForeignKey('Color', on_delete = models.SET_NULL, null = True)
@@ -116,7 +123,7 @@ class OrderedProduct(models.Model):
         db_table = 'ordered_products'
 
 class Order(models.Model):
-    user            = models.ForeignKey('User', on_delete = models.SET_NULL, null = True)
+    user            = models.ForeignKey(User, on_delete = models.SET_NULL, null = True)
     shipping        = models.ForeignKey('Shipping', on_delete = models.SET_NULL, null = True)
     created_at      = models.DateTimeField(auto_now_add = True)
     updated_at      = models.DateTimeField(auto_now = True)
