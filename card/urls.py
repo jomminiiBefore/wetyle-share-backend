c65badc6fa2268e34124604ddbb32015fa31638f
이종민 <>
2020-03-04 09:31:53 +0900

    컬렉션 업로드 기능 추가, 스타일 이미지 업로드 기능을 컬렉션에서도 사용할 수 있게 view 이름 변경

--- card/urls.py
+++ card/urls.py
@@ -9,20 +9,22 @@ from .views import (
     PopularCardView,
     CollectionView,
     CollectionFollowView,
-    StyleImageUploadView,
-    SearchCollectionView
+    ImageUploadView,
+    SearchCollectionView,
+    CollectionUploadView,
 )
 
 urlpatterns = [
     path('/style/<int:style_id>', StyleView.as_view()),
     path('/dailylook', DailyLookCardView.as_view()),
     path('/style/upload', StyleUploadView.as_view()),
-    path('style/upload/image', StyleImageUploadView.as_view()),
+    path('/upload/image', ImageUploadView.as_view()),
     path('/style/comment/<int:style_id>', StyleCommentView.as_view()),
     path('/style/like/<int:style_id>', StyleLikeView.as_view()),
     path('/popular', PopularCardView.as_view()),
     path('/collection/<int:collection_id>', CollectionView.as_view()),
     path('/collection/follow/<int:collection_id>', CollectionFollowView.as_view()),
-    path('collection/search', SearchCollectionView.as_view()),
-    path('dailylook/collection/', DailyLookCollectionView.as_view()),
+    path('/collection/search', SearchCollectionView.as_view()),
+    path('/dailylook/collection/', DailyLookCollectionView.as_view()),
+    path('/collection/upload', CollectionUploadView.as_view()),
 ]
