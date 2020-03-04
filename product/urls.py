9c38b48613b1a7e8e017abd171c39141655ce8e4
이종민 <>
2020-03-04 23:36:31 +0900

    상품 검색 기능 작성

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
-from .views import BrandListView
+from .views import BrandListView, SearchProductView
 
 from django.urls import path
 
 urlpatterns =[
     path('/brand-list', BrandListView.as_view()),
+    path('/search', SearchProductView.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -4,8 +4,22 @@ from .models import Brand
 
 from django.views import View
 from django.http import JsonResponse, HttpResponse
+from django.db.models import Count, Q
 
 class BrandListView(View):
     def get(self, request):
         return JsonResponse({"brand list": list(Brand.objects.values())}, status = 200)
 
+class SearchProductView(View):
+    def get(self, request):
+        query = request.GET.get('query', None)
+        searched_list = Product.objects.filter(Q(name__icontains = query) | Q(brand__name__icontains = query)).all()
+        product_list = [
+            {
+                "product_image_url" : product.image_url ,
+                "brand_name"        : product.brand.name ,
+                "name"              : product.name,
+                "price"             : product.price,
+                "discounted_price"  : product.discounted_price,
+            } for product in searched_list]
+        return JsonResponse({"product_list": product_list}, status = 200)
