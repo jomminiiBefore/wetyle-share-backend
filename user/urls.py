010740dd6085557d39989664524edb4972f1fac1
yongjunlee <chesidyong@gmail.com>
2020-03-06 12:03:52 +0900

    Feature/Add realtime checks fot id in database

--- user/urls.py
+++ user/urls.py
@@ -1,21 +1,19 @@
 from user.views import (
     SignInView,
     SignUpView,
-    CheckIdView,
-    CheckEmailView,
     CheckSignInIdView,
+    CheckSignUpIdView,
     UserFollowView,
-    KakaoSignInView)
+    KakaoSignInView
+    )
 
 from django.urls import path
 
 urlpatterns = [
     path('/sign-up', SignUpView.as_view()),
-    path('/sign-up/checkid', CheckIdView.as_view()),
-    path('/sign-up/checkemail', CheckEmailView.as_view()),
+    path('/sign-up/checkid', CheckSignUpIdView.as_view()),    
     path('/sign-in', SignInView.as_view()),
     path('/follow/<int:followee_id>', UserFollowView.as_view()), 
     path('/sign-in/checkid', CheckSignInIdView.as_view()),
     path('/kakao/sign-in', KakaoSignInView.as_view()),
 ]
-
